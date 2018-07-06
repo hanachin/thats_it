f9910c2c182358c99c3c3a023a3b06e25f27b8c6
Seiei Miyagi <hanachin@gmail.com>
2018-07-06 14:28:57 +0900

    Remove unnecessary comment

--- ext/implicit_it/implicit_it.c
+++ ext/implicit_it/implicit_it.c
@@ -15,7 +15,6 @@ static int implicit_it_block_p(const rb_iseq_t *iseq) {
 }
 
 static VALUE it() {
-  /* trunk 10485be8c2acd97b165c5d6bd9e53122a821562d */
   const rb_control_frame_t *cfp = ruby_current_execution_context_ptr->cfp;
   return *(cfp + 2)->sp;
 }
