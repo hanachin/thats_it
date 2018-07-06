7bfd2ca20ce256feacfe9a4449857e834b44216d
Seiei Miyagi <hanachin@gmail.com>
2018-07-06 14:50:04 +0900

    Remove unnecessary comment and blank line

--- lib/implicit_it.rb
+++ lib/implicit_it.rb
@@ -2,13 +2,11 @@ require "implicit_it/version"
 require "implicit_it/implicit_it"
 
 module ImplicitIt
-  # Your code goes here...
 end
 
 TracePoint.trace(:call) do |tp|
   setup_it_block_call
 end
-
 
 TracePoint.trace(:c_call) do |tp|
   setup_it_block_c_call
